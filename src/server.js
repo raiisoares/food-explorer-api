0b39393886651ea24682f0ccddb1280ddf5c1b8c
Raí Soares <raisoares.dev@gmail.com>
2023-08-21 23:00:30 -0300

    Routes and AppError

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { request, response, json } = require("express");
+const routes = require("./routes")
 
 const app = express();
 const PORT = 3333;
 
+app.use(express.json());
+app.use(routes);
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
