7c53b65989e1d093c6178d1fb71c554b01ba06a2
Raí Soares <rai_micaias@hotmail.com>
2023-10-13 11:43:51 -0300

    web domain added to cors

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://foodexpweb.netlify.app/"],
     credentials: true,
   })
 );
