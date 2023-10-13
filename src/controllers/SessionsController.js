201ecf1082de41326ec10a5c462f8a0d7639d2f0
Raí Soares <rai_micaias@hotmail.com>
2023-10-13 17:19:26 -0300

    signIn error response fixed

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -2,6 +2,7 @@ const SessionRepository = require("../repositories/SessionRepository");
 const SessionCreateService = require("../services/SessionCreateService");
 const { sign } = require("jsonwebtoken");
 const authConfig = require("../configs/auth");
+const AppError = require("../utils/AppError");
 
 class SessionController {
   async create(request, response) {
@@ -9,6 +10,7 @@ class SessionController {
       const { email, password } = request.body;
       const sessionRepository = new SessionRepository();
       const sessionCreateService = new SessionCreateService(sessionRepository);
+
       const user = await sessionCreateService.execute({ email, password });
 
       const { secret, expiresIn } = authConfig.jwt;
@@ -27,7 +29,7 @@ class SessionController {
 
       response.status(201).json({ user });
     } catch (error) {
-      console.log(error);
+      throw new AppError(error.message, error.status);
     }
   }
 }
--- src/services/SessionCreateService.js
+++ src/services/SessionCreateService.js
@@ -1,13 +1,15 @@
 class SessionCreateService {
+  constructor(sessionRepository) {
+    this.sessionRepository = sessionRepository;
+  }
 
-    constructor(productsRepository) {
-        this.productsRepository = productsRepository;
-    }
-
-    async execute({email, password}) {
-        const sessionCreated = await this.productsRepository.create({email, password});
-        return sessionCreated;
-    }
+  async execute({ email, password }) {
+    const sessionCreated = await this.sessionRepository.create({
+      email,
+      password,
+    });
+    return sessionCreated;
+  }
 }
 
 module.exports = SessionCreateService;
